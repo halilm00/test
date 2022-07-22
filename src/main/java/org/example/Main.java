f75a2dd9e912bc796809f852790a05e0c307a42f
halilm00 <halilmehmed2000@gmail.com>
2022-07-22 10:47:09 +0300

    create Person class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("wow");
+
+        Person person = new Person("Ivan",5);
+        System.out.println("person = " + person);
     }
 }
